e5b6658f9935a6f4026e2723c08f8cae6a41f32e
TarnJu-Han <tarnju-han@TarnJu-HandeMacBook-Air.local>
2018-01-02 17:17:34 +0800

    added hw05

--- pointers.c
+++ pointers.c
@@ -8,21 +8,45 @@
 #include <stdlib.h>
 
 #include "pointers.h"
-
 /* Returns whether or not the region pointed to by p1 (of length n)
    overlaps with the region pointed to by p2 (of length m). Do not
    assume that p2 is greater than p1. */
 bool overlaps(int n, int* p1, int m, int* p2)
 {
   /* WRITE THIS FUNCTION */
-  return false;
+  if(p1 < p2){
+    return ((p1+n) > p2);
+  }
+  else{
+    return ((p2 + m) > p1);
+  }
 }
 
 /* Changes the value in *a, *b, and *c so that *a, *b, and *c are
    in non-decreasing order. */
-void minisort(int* a, int* b, int* c)
+void minisort(int* a, int* b, int* c) //x = 4, y=1, z=3
 {
   /* WRITE THIS FUNCTION */
+  int small, medium, big;
+  if(*a > *b){
+    big = *a;
+    small = *b ;
+  }else{
+    big = *b;
+    small = *a;
+  }
+  if(*c < small){
+    medium = small;
+    small = *c;
+  }else if(*c > big){
+    medium = big;
+    big = *c ;
+  }else{
+    medium = *c ;
+  }
+  *a = small;
+  *b = medium;
+  *c = big;
 }
 
 /* Computes the sum of the ints that sit in memory between p
@@ -31,7 +55,12 @@ void minisort(int* a, int* b, int* c)
 int partial_sum(int* p, int* q)
 {
   /* WRITE THIS FUNCTION */
-  return 0;
+  int sum = 0;
+  while(p < q){
+    sum = sum + *p ;
+    p++;
+  }
+  return sum;
 }
 
 /* Check whether `n` array elements, starting at `start` and
@@ -41,7 +70,12 @@ int partial_sum(int* p, int* q)
 int check_line(int* start, int step, int n)
 {
   /* WRITE THIS FUNCTION */
-  return 0;
+  for(int i = step; i<step*n; i = i + step){
+    if(*start != *(start+i)){ //if start[0] != start[i]
+      return BLANK;
+    }
+  }
+  return start[0];
 }
 
 /* Check for a winner of Tic Tac Toe. Precondition:
@@ -51,5 +85,21 @@ int check_line(int* start, int step, int n)
 int tic_tac_toe_winner(int n, int* board)
 {
   /* WRITE THIS FUNCTION */
-  return 0;
+  int result;
+  //case 1: row
+  for(int i = 0; i<n; i++){
+    result = check_line((board+i*n), 1, n);
+  }
+
+  //case 2: column
+  for(int i = 0; i<n; i++){
+    result = check_line((board+i), n, n);
+  }
+
+  //case 3: left to right diagonal
+  result = check_line(board, n+1, n);
+
+  //case 4: right to left diagonal
+  result = check_line((board + (n-1)), n-1, n);
+  return result;
 }
